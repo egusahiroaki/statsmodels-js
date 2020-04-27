bd749abd4854131aa024ef652409e0ff4bce11e8
egusahiroaki <egusa.hiroaki@gmail.com>
2020-04-27 21:18:19 +0900

    test case 2

--- index.test.js
+++ index.test.js
@@ -1,8 +1,15 @@
 const LinearRegression = require("./index");
 
-test("test", () => {
+test("conef: 1, intercept: 0", () => {
   const l = new LinearRegression();
   const x = [1, 2, 3];
   const y = [1, 2, 3];
   expect(l.fit(x, y)).toEqual({ coef: 1, intercept: 0 });
+});
+
+test("conef: 1, intercept: 0", () => {
+  const l = new LinearRegression();
+  const x = [0, 3, 6];
+  const y = [1, 2, 3];
+  expect(l.fit(x, y)).toEqual({ coef: 1 / 3, intercept: 1 });
 });
