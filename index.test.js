77dc56b6593036dc345af6b349e5c0fae71d0a39
egusahiroaki <egusa.hiroaki@gmail.com>
2020-04-27 21:32:11 +0900

    fix test description

--- index.test.js
+++ index.test.js
@@ -7,7 +7,7 @@ test("conef: 1, intercept: 0", () => {
   expect(l.fit(x, y)).toEqual({ coef: 1, intercept: 0 });
 });
 
-test("conef: 1, intercept: 0", () => {
+test("conef: 1/3, intercept: 1", () => {
   const l = new LinearRegression();
   const x = [0, 3, 6];
   const y = [1, 2, 3];
