900d9d43582f5273777e8c2511e7c22b2e1ac2e3
egusahiroaki <egusa.hiroaki@gmail.com>
2020-04-27 21:05:45 +0900

    implement

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const dot = require('./util');
+const _ = require('lodash');
+
+
 class LinearRegression {
     constructor(){    
     }
 
     fit(x,y) {
-        return x
+        const n = x.length
+
+        const a = ((dot(x,y) - _.sum(y) * _.sum(x) / n) / 
+            ( _.sum(_.map(x, (e)=>{ return e * e})) - _.sum(x)**2 / n ))
+
+        const b = (_.sum(y) - a* _.sum(x)) / n
+        return {
+            coef: a,
+            intercept: b
+        }
     }
 }
 
--- index.test.js
+++ index.test.js
@@ -2,6 +2,7 @@ const LinearRegression = require('./index');
 
 test('test', () => {
     const l = new LinearRegression()
-    
-    expect(l.fit(1,2)).toBe(1);
+    const x = [1,2,3]
+    const y = [1,2,3]
+    expect(l.fit(x,y)).toEqual({"coef": 1, "intercept": 0})
   });
