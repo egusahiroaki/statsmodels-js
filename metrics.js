5463586fe4c2de0bcfea6b2cfcfabf253ddc7e4a
egusahiroaki <egusa.hiroaki@gmail.com>
2020-04-28 20:54:20 +0900

    assert

--- metrics.js
+++ metrics.js
@@ -2,7 +2,10 @@ import _ from "lodash";
 
 // Mean squared error regression loss
 const meanSquaredError = (yTrue, yPred) => {
-  // TODO: assert Size
+  if (yTrue.length !== yPred.length) {
+    throw new Error("The size of both arrays should be same.");
+  }
+
   return (
     _.sum(_.map(yTrue, (e, i) => Math.pow(yTrue[i] - yPred[i], 2))) /
     yTrue.length
--- metrics.test.js
+++ metrics.test.js
@@ -2,6 +2,14 @@ import { meanSquaredError } from "./metrics";
 
 describe("meanSquaredError", () => {
   test("", () => {
+    const yTrue = [1];
+    const yPred = [2, 3, 4];
+    expect(() => {
+      meanSquaredError(yTrue, yPred);
+    }).toThrowError(new Error("The size of both arrays should be same."));
+  });
+
+  test("", () => {
     const yTrue = [3, -0.5, 2, 7];
     const yPred = [2.5, 0.0, 2, 8];
 
