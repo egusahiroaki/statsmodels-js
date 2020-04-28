2c14018d3d0fdfeef118f462b676e0329a80f24c
egusahiroaki <egusa.hiroaki@gmail.com>
2020-04-28 21:07:12 +0900

    add squared option

--- metrics.js
+++ metrics.js
@@ -1,15 +1,14 @@
 import _ from "lodash";
 
 // Mean squared error regression loss
-const meanSquaredError = (yTrue, yPred) => {
+const meanSquaredError = (yTrue, yPred, options = { squared: true }) => {
   if (yTrue.length !== yPred.length) {
     throw new Error("The size of both arrays should be same.");
   }
-
-  return (
+  const result =
     _.sum(_.map(yTrue, (e, i) => Math.pow(yTrue[i] - yPred[i], 2))) /
-    yTrue.length
-  );
+    yTrue.length;
+  return options.squared ? result : Math.sqrt(result);
 };
 
 module.exports = { meanSquaredError };
--- metrics.test.js
+++ metrics.test.js
@@ -15,4 +15,13 @@ describe("meanSquaredError", () => {
 
     expect(meanSquaredError(yTrue, yPred)).toEqual(0.375);
   });
+
+  test("", () => {
+    const yTrue = [3, -0.5, 2, 7];
+    const yPred = [2.5, 0.0, 2, 8];
+
+    expect(meanSquaredError(yTrue, yPred, { squared: false })).toEqual(
+      0.6123724356957945
+    );
+  });
 });
