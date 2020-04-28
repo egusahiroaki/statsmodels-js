7c7d01e684da61b27337a0c1cd916fd5625ce0cb
egusahiroaki <egusa.hiroaki@gmail.com>
2020-04-28 21:36:15 +0900

    test case

--- metrics.test.js
+++ metrics.test.js
@@ -28,6 +28,18 @@ describe("meanSquaredError", () => {
 
 describe("r2Score", () => {
   test("", () => {
+    const yTrue = [1, 2];
+    const yPred = [1, 2];
+    expect(r2Score(yTrue, yPred)).toEqual(1);
+  });
+
+  test("", () => {
+    const yTrue = [1, 2];
+    const yPred = [0.9, 2.1];
+    expect(r2Score(yTrue, yPred)).toEqual(0.96);
+  });
+
+  test("", () => {
     const yTrue = [3, -0.5, 2, 7];
     const yPred = [2.5, 0.0, 2, 8];
     expect(r2Score(yTrue, yPred)).toEqual(0.9486081370449679);
