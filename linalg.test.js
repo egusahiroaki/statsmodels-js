74c8b8ec888fa9436eba4ff492a0614f4780e377
egusahiroaki <egusa.hiroaki@gmail.com>
2020-04-28 09:53:44 +0900

    add describe

--- linalg.test.js
+++ linalg.test.js
@@ -1,45 +1,49 @@
 import { matrixRank, solve } from "./linalg";
 
-test("", () => {
-  const a = [
-    [1, 1, 4, 1],
-    [0, 3, 1, 1],
-    [0, 6, 2, 3],
-  ];
+describe("matrixRank", () => {
+  test("", () => {
+    const a = [
+      [1, 1, 4, 1],
+      [0, 3, 1, 1],
+      [0, 6, 2, 3],
+    ];
 
-  expect(matrixRank(a)).toEqual(3);
+    expect(matrixRank(a)).toEqual(3);
+  });
+
+  test("", () => {
+    const a = [
+      [1, 1, 4, 1],
+      [0, 3, 1, 1],
+      [0, 6, 2, 2],
+    ];
+
+    expect(matrixRank(a)).toEqual(2);
+  });
+
+  test("", () => {
+    const a = [
+      [1, 1, 4, 1, 0],
+      [1, 1, 4, 1, 1],
+      [0, 3, 1, 1, 1],
+      [0, 6, 2, 2, 2],
+    ];
+
+    expect(matrixRank(a)).toEqual(3);
+  });
 });
 
-test("", () => {
-  const a = [
-    [1, 1, 4, 1],
-    [0, 3, 1, 1],
-    [0, 6, 2, 2],
-  ];
+describe("solve", () => {
+  test("", () => {
+    const A = [
+      [1, 0, 0, 0],
+      [0, 2, 0, 0],
+      [0, 0, 3, 0],
+      [0, 0, 0, 4],
+    ];
 
-  expect(matrixRank(a)).toEqual(2);
-});
-
-test("", () => {
-  const a = [
-    [1, 1, 4, 1, 0],
-    [1, 1, 4, 1, 1],
-    [0, 3, 1, 1, 1],
-    [0, 6, 2, 2, 2],
-  ];
-
-  expect(matrixRank(a)).toEqual(3);
-});
-
-test("", () => {
-  const A = [
-    [1, 0, 0, 0],
-    [0, 2, 0, 0],
-    [0, 0, 3, 0],
-    [0, 0, 0, 4],
-  ];
-
-  const b = [-1, -1, -1, -1];
-  const expected = [[-1], [-0.5], [-1 / 3], [-0.25]];
-  expect(solve(A, b)).toEqual(expected);
+    const b = [-1, -1, -1, -1];
+    const expected = [[-1], [-0.5], [-1 / 3], [-0.25]];
+    expect(solve(A, b)).toEqual(expected);
+  });
 });
