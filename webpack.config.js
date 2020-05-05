1cf211c0ba2a4f71249fc40fabf32bab8b72b790
egusahiroaki <egusa.hiroaki@gmail.com>
2020-05-05 15:32:27 +0900

    Revert "use file size optimize"
    This reverts commit 9e03e77229d206da7c12a8d3c3605d299eca23bc.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-const TerserPlugin = require("terser-webpack-plugin");
-
 module.exports = {
   mode: "development",
   devtool: "source-map",
@@ -20,22 +18,6 @@ module.exports = {
           loader: "babel-loader",
         },
       },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          ecma: 6,
-          compress: true,
-          output: {
-            comments: false,
-            beautify: false,
-            drop_console: true,
-          },
-        },
-      }),
     ],
   },
 };
